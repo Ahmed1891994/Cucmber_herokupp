e7b813acff5c7cf79035fef987026c497a6522af
Ahmed1891994 <ahmedhakem189@yahoo.com>
2021-09-11 21:58:29 +0200

    Add Cucumber features + step definitions

--- src/test/java/herokupp/cu/features/MultipleWindowsPage.feature
+++ src/test/java/herokupp/cu/features/MultipleWindowsPage.feature
@@ -1,37 +1,7 @@
 Feature: Multi windows Functionality
 Testing Multi windows
 
-Scenario: Open new window Successfully
 
-When user enters multiplewindowspage
-And user Clicks on clickme
-And user Switches to new window
-Then Check new open window
-#####################################################################################
-Scenario: Close new window and return to original one
-
-When user enters multiplewindowspage
-And user Clicks on clickme
-And user Switches to new window
-Then Check new open window
-And user closes new window
-Then Check original window
-
-
-#####################################################################################
-Scenario: Open new window two times Successfully
-
-When user enters multiplewindowspage
-And user Clicks on clickme
-And user Switches to new window
-Then Check new open window
-And user closes new window
-Then Check original window
-And user Clicks on clickme
-And user Switches to new window
-Then Check new open window
-And user closes new window
-Then Check original window
 #####################################################################################
 Scenario: Open new multiple windows Successfully
 
---
+++ test-output/report.js
@@ -0,0 +1,329 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/herokupp/cu/features/MultipleWindowsPage.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Multi windows Functionality",
+  "description": "Testing Multi windows",
+  "id": "multi-windows-functionality",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 13093380100,
+  "status": "passed"
+});
+formatter.scenario({
+  "comments": [
+    {
+      "line": 5,
+      "value": "#####################################################################################"
+    }
+  ],
+  "line": 6,
+  "name": "Open new multiple windows Successfully",
+  "description": "",
+  "id": "multi-windows-functionality;open-new-multiple-windows-successfully",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters multiplewindowspage",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "user Clicks on clickme",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "user Clicks on clickme",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user Clicks on clickme",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "user Clicks on clickme",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "user Clicks on clickme",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "user Switches to new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "Check new open window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 16,
+  "name": "user closes new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "Check original window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "user Switches to new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "Check new open window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "user closes new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "Check original window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 22,
+  "name": "user Switches to new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "Check new open window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 24,
+  "name": "user closes new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "Check original window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 26,
+  "name": "user Switches to new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "Check new open window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 28,
+  "name": "user closes new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "Check original window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 30,
+  "name": "user Switches to new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "Check new open window",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 32,
+  "name": "user closes new window",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "Check original window",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_enters_multiplewindowspage()"
+});
+formatter.result({
+  "duration": 683823000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Clicks_on_clickme()"
+});
+formatter.result({
+  "duration": 518812000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Clicks_on_clickme()"
+});
+formatter.result({
+  "duration": 261898300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Clicks_on_clickme()"
+});
+formatter.result({
+  "duration": 89693200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Clicks_on_clickme()"
+});
+formatter.result({
+  "duration": 144899800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Clicks_on_clickme()"
+});
+formatter.result({
+  "duration": 122301600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Switches_to_new_window()"
+});
+formatter.result({
+  "duration": 221223700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_new_open_window()"
+});
+formatter.result({
+  "duration": 316322300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_closes_new_window()"
+});
+formatter.result({
+  "duration": 54167200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_original_window()"
+});
+formatter.result({
+  "duration": 57927000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Switches_to_new_window()"
+});
+formatter.result({
+  "duration": 103886700,
+  "error_message": "java.util.NoSuchElementException\r\n\tat java.util.LinkedHashMap$LinkedHashIterator.nextNode(LinkedHashMap.java:721)\r\n\tat java.util.LinkedHashMap$LinkedKeyIterator.next(LinkedHashMap.java:742)\r\n\tat herokupp.cu.pages.MultipleWindowsPage.SwitchtoNextWindow(MultipleWindowsPage.java:126)\r\n\tat herokupp.cu.stepdefinitions.MultipleWindowsTestSteps.user_Switches_to_new_window(MultipleWindowsTestSteps.java:28)\r\n\tat ✽.And user Switches to new window(src/test/java/herokupp/cu/features/MultipleWindowsPage.feature:18)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_new_open_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_closes_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_original_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Switches_to_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_new_open_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_closes_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_original_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Switches_to_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_new_open_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_closes_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_original_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_Switches_to_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_new_open_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.user_closes_new_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "MultipleWindowsTestSteps.Check_original_window()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 26100,
+  "status": "passed"
+});
+});
